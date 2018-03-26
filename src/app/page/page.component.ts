3922c22123f3ff446e9f8623b0a5958c42900c15
Collins Muiruri <wanyekicollins@gmail.com>
2018-03-26 11:41:48 +0300

    Add html

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GithubService} from '../user/github.service';
 @Component({
-  selector: 'app-page',
-  templateUrl: './page.component.html',
-  styleUrls: ['./page.component.css']
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
 })
-export class PageComponent implements OnInit {
+export class GithubComponent implements OnInit {
 
-  constructor() { }
+  user: any[];
+  repos: any[];
+  username:string;
+  constructor(private githubService: GithubService) {
+   }
+
+   searchUser(){
+     this.githubService.updateUser(this.username);
+     this.githubService.getUserInfo().subscribe(user =>{
+       console.log(user);
+       this.user = user;
+     });
+     this.githubService.getUserRepos().subscribe(repos =>{
+       console.log(repos);
+       this.repos = repos;
+     });
+   }
 
   ngOnInit() {
   }
